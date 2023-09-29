c93ccb1f898467cb54cca37f2cabc70a626e1232
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-29 11:09:07 +0600

    Add New Option

--- resources/views/admin/user/user_information.blade.php
+++ resources/views/admin/user/user_information.blade.php
@@ -8,43 +8,44 @@
 	</div>
 	<div class="card radius-10">
 		<div class="card-body">
-			<div class="d-flex align-items-center">
-				<div>
-					<h5 style="font-family: 'Times New Roman', Times, serif;font-style:italic;font-size:40px;color:rgb(6, 38, 249);text-align:center;" class="mb-0">All User's Information</h5>
-				</div>
+			<div class=" align-items-center">
+				<div class="row">
+					<div style="width: 50%;padding-top:1.5rem;" class="column"><h5 style="font-family: 'Times New Roman', Times, serif;font-style:italic;font-size:40px;color:rgb(6, 38, 249);text-align:center;" class="mb-0">All Worker's Information</h5></div>
+					<div style="width: 50%;padding-top:2rem;padding-left:22rem;" class="column"><a href='{{ route ('add.new')}}' style="font-family: 'Times New Roman', Times, serif;font-style:bold;color:white;font-size:20px;cursor:pointer;" class="btn btn-success" >ADD NEW WORKER</a></div>
 				</div>
 			</div>
-			<hr>
-			<div class="table-responsive">
-				<table class="table align-middle mb-0">
-					<thead class="table-light">
-						<tr>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Sl</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Photo</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Name</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Email</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Status</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">join Date</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Payment Information</th>
-							<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Action</th> 
-						</tr>
-					</thead>
+		</div>
+		<hr>
+		<div class="table-responsive">
+			<table class="table align-middle mb-0">
+				<thead class="table-light">
+					<tr>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Sl</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Photo</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Name</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Email</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Status</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">join Date</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Payment Information</th>
+						<th style="font-family: 'Times New Roman', Times, serif;font-size:15px;text-align:center;">Action</th> 
+					</tr>
+				</thead>
 					@foreach ($alluser as $alluser)
-						<tbody>
-							<tr>
-								<td>{{$alluser->id}}</td>
-								<td><img src="{{ asset($alluser->photo)}}" height="70px" width="70px" /></td>
-								<td>{{$alluser->name}}</td>
-								<td>{{$alluser->email}}</td>
-								<td>{{$alluser->status}}</td>
-								<td>{{$alluser->created_at}}</td>
-								<td>{{$alluser->payment_info}}</td>
-								<td>
-									<a href='{{ route ('user.information.edit',$alluser->id)}}' style="font-family: 'Times New Roman', Times, serif;font-style:bold;color:white;font-size:20px;cursor:pointer;" class="btn btn-success" >UPDATE</a>
-									<a href='{{ route ('user.delete',$alluser->id)}}'style="font-family: 'Times New Roman', Times, serif;font-style:bold;font-size:20px;cursor:pointer;color:white;" id="delete" class="btn btn-danger" >DELETE</a>
-								</td>
-							</tr>
-						</tbody>
+					<tbody>
+						<tr>
+							<td>{{$alluser->id}}</td>
+							<td><img src="{{ asset($alluser->photo)}}" height="70px" width="70px" /></td>
+							<td>{{$alluser->name}}</td>
+							<td>{{$alluser->email}}</td>
+							<td>{{$alluser->status}}</td>
+							<td>{{$alluser->created_at}}</td>
+							<td>{{$alluser->payment_info}}</td>
+							<td>
+								<a href='{{ route ('user.information.edit',$alluser->id)}}' style="font-family: 'Times New Roman', Times, serif;font-style:bold;color:white;font-size:20px;cursor:pointer;" class="btn btn-success" >UPDATE</a>
+								<a href='{{ route ('user.delete',$alluser->id)}}'style="font-family: 'Times New Roman', Times, serif;font-style:bold;font-size:20px;cursor:pointer;color:white;" id="delete" class="btn btn-danger" >DELETE</a>
+							</td>
+						</tr>
+					</tbody>
 					@endforeach
 				</table>
 			</div>
