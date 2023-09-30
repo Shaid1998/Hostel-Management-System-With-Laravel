692f9aeb54b44ed4ec583a0838af6eefd4494add
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-30 20:03:35 +0600

    Other Index Page Create And Design

--- resources/views/admin/other/other_index.blade.php
+++ resources/views/admin/other/other_index.blade.php
@@ -3,112 +3,52 @@
  
 
 <div class="page-content">
-	<div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
+	<div >
 		<div class="col">
-			<div class="card radius-10 bg-gradient-deepblue">
-				<div class="card-body">
-					<div class="d-flex align-items-center">
-						<h5 class="mb-0 text-white">2</h5>
-						<div class="ms-auto">
-							<i class='fas fa-user-cog fs-3 text-white'></i>
-						</div>
-					</div>
-					<div class="progress my-3 bg-light-transparent" style="height:3px;">
-						<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-					</div>
-					<div class="d-flex align-items-center text-white">
-						<p class="mb-0">Total Admin</p>
-						<p class="mb-0 ms-auto">--<span><i class='bx bx-up-arrow-alt'></i></span></p>
-					</div>
-				</div>
-			</div>
+            <a href="{{route('admin.other.photo.option')}}">
+                <div style="height: 250px;width:100%;text-align:center" class="card radius-10 bg-gradient-deepblue">
+                    <div class="card-body">
+                        <div class=" align-items-center text-white">
+                            <h1 style="font-family: 'Times New Roman', Times, serif;padding-top:4rem;font-weight:900;font-style:italic;color:aliceblue;text-align:center;font-size:70px">UPLOAD NEW PHOTO TO GALLARY</h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 		<div class="col">
-			<div class="card radius-10 bg-gradient-orange">
-				<div class="card-body">
-					<div class="d-flex align-items-center">
-						<h5 class="mb-0 text-white">200000</h5>
-						<div class="ms-auto">
-							<i class='far fa-file-alt fs-3 text-white'></i>
-						</div>
-					</div>
-					<div class="progress my-3 bg-light-transparent" style="height:3px;">
-						<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-					</div>
-					<div class="d-flex align-items-center text-white">
-						<p class="mb-0">Total Post</p>
-						<p class="mb-0 ms-auto">+1.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
-					</div>
-				</div>
-			</div>
+            <a href="{{route('admin.other.photo.option')}}">
+                <div style="height: 250px;width:100%;text-align:center" class="card radius-10 bg-gradient-ibiza">
+                    <div class="card-body">
+                        <div class=" align-items-center text-white">
+                            <h1 style="font-family: 'Times New Roman', Times, serif;padding-top:4rem;font-weight:900;font-style:italic;color:aliceblue;text-align:center;font-size:70px">UPLOAD NEW SERVICE & UPDATE</h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 		<div class="col">
-			<div class="card radius-10 bg-gradient-ohhappiness">
-				<div class="card-body">
-					<div class="d-flex align-items-center">
-						<h5 class="mb-0 text-white">2000</h5>
-						<div class="ms-auto">
-							<i class='fas fa-user-alt fs-3 text-white'></i>
-						</div>
-					</div>
-					<div class="progress my-3 bg-light-transparent" style="height:3px;">
-						<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-					</div>
-					<div class="d-flex align-items-center text-white">
-						<p class="mb-0">Total User</p>
-						<p class="mb-0 ms-auto">+9.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
-					</div>
-				</div>
-			</div>
+            <a href="{{route('admin.other.photo.option')}}">
+                <div style="height: 250px;width:100%;text-align:center" class="card radius-10 bg-gradient-ohhappiness">
+                    <div class="card-body">
+                        <div class=" align-items-center text-white">
+                            <h1 style="font-family: 'Times New Roman', Times, serif;padding-top:4rem;font-weight:900;font-style:italic;color:aliceblue;text-align:center;font-size:70px">UPLOAD CONTACT & UPDATE</h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 		<div class="col">
-			<div class="card radius-10 bg-gradient-ibiza">
-				<div class="card-body">
-					<div class="d-flex align-items-center">
-						<h5 class="mb-0 text-white">200</h5>
-						<div class="ms-auto">
-							<i class='fas fa-user-alt fs-3 text-white'></i>
-						</div>
-					</div>
-					<div class="progress my-3 bg-light-transparent" style="height:3px;">
-						<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-					</div>
-					<div class="d-flex align-items-center text-white">
-						<p class="mb-0">Messages</p>
-						<p class="mb-0 ms-auto">+11.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
-					</div>
-				</div>
-			</div>
+            <a href="{{route('admin.other.photo.option')}}">
+                <div style="height: 250px;width:100%;text-align:center" class="card radius-10 bg-gradient-orange">
+                    <div class="card-body">
+                        <div class=" align-items-center text-white">
+                            <h1 style="font-family: 'Times New Roman', Times, serif;padding-top:4rem;font-weight:900;font-style:italic;color:aliceblue;text-align:center;font-size:70px">UPLOAD SITE HOME & UPDATE</h1>
+                        </div>
+                    </div>
+                </div>
+            </a>
 		</div>
 	</div><!--end row-->
-	<div class="card radius-10">
-		<div class="card-body">
-			<div class="d-flex align-items-center">
-				<div>
-					<h5 class="mb-0">Orders Summary</h5>
-				</div>
-				<div class="font-22 ms-auto"><i class="bx bx-dots-horizontal-rounded"></i>
-				</div>
-			</div>
-			<hr>
-			<div class="table-responsive">
-				<table class="table align-middle mb-0">
-					<thead class="table-light">
-						<tr>
-							<th>Sl</th>
-							<th>Date</th>
-							<th>Invoice</th>
-							<th>Amount</th>
-							<th>Payment</th>
-							<th>Status</th> 
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
 </div>
 
 @endsection
